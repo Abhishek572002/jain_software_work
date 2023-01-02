ce035059c5df488138abf7dbdba8af56b6039cf5
Abhishek Jaiswal <jaisawalabhi0102@gmail.com>
2023-01-02 23:08:16 +0530

    my project

---
+++ js/datatables-simple-demo.js
@@ -0,0 +1,7 @@
+window.addEventListener('DOMContentLoaded', event => {
+    // Simple-DataTables
+    const datatablesSimple = document.getElementById('datatablesSimple');
+    if (datatablesSimple) {
+        new simpleDatatables.DataTable(datatablesSimple);
+    }
+});
